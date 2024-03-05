63a27f2f48a40bacada13ee3bcf47a4bb5d93a1b
Divine <divine.ekb.dev@gmail.com>
2024-03-05 14:25:55 +0500

    fix: вернул шрифты в конфиг

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Roboto: true,
+        Inter: [400, 700],
+        'Josefin+Sans': true,
+        Lato: [100, 300],
+        Raleway: {
+          wght: [100, 400],
+          ital: [100]
+        },
+        'Crimson Pro': {
+          wght: '200..900',
+          ital: '200..700',
+        },
+        Manrope: [400, 500, 600, 700]
+      }
+    }],
+  ],
   router: {
     routes: [
       {
