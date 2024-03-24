b334a59f2575f38d296ab630ef16b02dd1029b58
Artem Kiselev <kuzuru.dev@gmail.com>
2024-03-25 03:32:27 +0500

    feat(middleware): JWT

---
+++ dbworker/middleware/token.go
@@ -0,0 +1,158 @@
+package middleware
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Config struct {
+	// When returned true, our middleware is skipped
+	Filter func(c *fiber.Ctx) bool
+
+	// Function to run when there is error decoding jwt
+	Unauthorized fiber.Handler
+
+	// Function to decode our jwt token
+	Decode func(c *fiber.Ctx) (*jwt.MapClaims, error)
+
+	// JWT secret
+	Secret string
+
+	// JWT expiry in seconds
+	Expiry int64
+}
+
+// ConfigDefault is middleware specific default config if not passed
+var ConfigDefault = Config{
+	Filter:       nil,
+	Decode:       nil,
+	Unauthorized: nil,
+	Secret:       "a_very_weak_secret", // TODO: Replace with .env value
+	Expiry:       60,
+}
+
+// configDefault is a function for generating default config
+func configDefault(config ...Config) Config {
+	// Return default config if nothing provided
+	if len(config) < 1 {
+		return ConfigDefault
+	}
+
+	// Override default config
+	cfg := config[0]
+
+	// Set default values if not passed
+	if cfg.Filter == nil {
+		cfg.Filter = ConfigDefault.Filter
+	}
+
+	// Set default secret if not passed
+	if cfg.Secret == "" {
+		cfg.Secret = ConfigDefault.Secret
+	}
+
+	// Set default expiry if not passed
+	if cfg.Expiry == 0 {
+		cfg.Expiry = ConfigDefault.Expiry
+	}
+
+	// this is the main jwt decode function of our middleware
+	if cfg.Decode == nil {
+		// Set default Decode function if not passed
+		cfg.Decode = func(c *fiber.Ctx) (*jwt.MapClaims, error) {
+
+			authHeader := c.Get("Authorization")
+
+			if authHeader == "" {
+				return nil, errors.New("authorization header is required")
+			}
+
+			// we parse our jwt token and check for validity against our secret
+			token, err := jwt.Parse(
+				authHeader[7:],
+				func(token *jwt.Token) (interface{}, error) {
+					// verifying our algo
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+					}
+					return []byte(cfg.Secret), nil
+				},
+			)
+
+			if err != nil {
+				return nil, errors.New("error parsing token")
+			}
+
+			claims, ok := token.Claims.(jwt.MapClaims)
+
+			if !(ok && token.Valid) {
+				return nil, errors.New("Invalid token")
+			}
+
+			if expiresAt, ok := claims["exp"]; ok && int64(expiresAt.(float64)) < time.Now().UTC().Unix() {
+				return nil, errors.New("jwt is expired")
+			}
+
+			return &claims, nil
+		}
+	}
+
+	// Set default Unauthorized if not passed
+	if cfg.Unauthorized == nil {
+		cfg.Unauthorized = func(c *fiber.Ctx) error {
+			return c.SendStatus(fiber.StatusUnauthorized)
+		}
+	}
+
+	return cfg
+}
+
+// Encode is a function to generate a jwt token
+func Encode(claims *jwt.MapClaims, secret string, expiryAfter int64) (string, error) {
+	// setting default expiryAfter
+	if expiryAfter == 0 {
+		expiryAfter = ConfigDefault.Expiry
+	}
+
+	// or you can use time.Now().Add(time.Second * time.Duration(expiryAfter)).UTC().Unix()
+	(*claims)["exp"] = time.Now().UTC().Unix() + expiryAfter
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// our signed jwt token string
+	signedToken, err := token.SignedString([]byte(ConfigDefault.Secret))
+
+	if err != nil {
+		return "", errors.New("error creating a token")
+	}
+
+	return signedToken, nil
+}
+
+// New is REQUIRED for every middleware
+func New(config Config) fiber.Handler {
+	// For setting default config
+	cfg := configDefault(config)
+
+	return func(c *fiber.Ctx) error {
+		// Don't execute middleware if Filter returns true
+		if cfg.Filter != nil && cfg.Filter(c) {
+			fmt.Println("Midddle was skipped")
+			return c.Next()
+		}
+		fmt.Println("Midddle was run")
+
+		claims, err := cfg.Decode(c)
+
+		if err == nil {
+			c.Locals("jwtClaims", *claims)
+			return c.Next()
+		}
+
+		return cfg.Unauthorized(c)
+	}
+}
