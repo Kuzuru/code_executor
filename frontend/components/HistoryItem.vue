5ca8b9e72dca32780d048110d2e7246a64338451
Divine <divine.ekb.dev@gmail.com>
2024-04-01 20:48:37 +0500

    feat: сверстал шаблон страницы истории

---
+++ frontend/components/HistoryItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="history-item">
+    <p class="history-item__date">29.03.2024</p>
+    <p class="history-item__time">7:40</p>
+    <p class="history-item__action">Удален</p>
+    <p class="history-item__file-name">MainHelloWorld.java </p>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style>
+@import "../assets/HistoryItem.css";
+</style>
---
+++ frontend/pages/history.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="history">
+    <HistoryItem/>
+    <HistoryItem/>
+    <HistoryItem/>
+  </section>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+@import "../assets/History.css";
+</style>
