3c2ab477537bb92e1c7d89ac4fb980e8e08b4230
Divine <divine.ekb.dev@gmail.com>
2024-04-01 20:48:02 +0500

    feat: сверстал шаблон профиля

---
+++ frontend/pages/profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="profile">
+    <div class="profile__personal">
+      <h2 class="profile__title">Личные данные</h2>
+      <h3 class="profile__subtitle">Имя</h3>
+
+      <p class="profile__data">Иван</p>
+      <h3 class="profile__subtitle">Логин</h3>
+      <p class="profile__data">Ivan228</p>
+    </div>
+
+    <div class="profile__safety">
+      <h2 class="profile__title">Безопасность</h2>
+      <h3 class="profile__subtitle">Пароль</h3>
+      <p class="profile__data" id="password">******</p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+</script>
+
+<style>
+@import '../assets/Profile.css';
+</style>
