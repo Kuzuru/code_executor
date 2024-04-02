ccc2113bfa99ac0ae9a62d24710383b49eb98884
Divine <divine.ekb.dev@gmail.com>
2024-04-02 12:35:09 +0500

    feat: сверстал страничку login и register, добавил пустой layout

---
+++ frontend/pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="login">
+    <h2 class="login__title">Вход</h2>
+    <input class="login__data" id="login" placeholder="Login">
+    <input class="login__data" id="password" placeholder="Password">
+    <div class="login__issue">
+      <p class="login__issue_text">Еще нет акаунта?</p>
+      <nuxt-link class="login__issue_link" to="/register">Регистрация</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+definePageMeta({
+  layout: 'empty'
+})
+</script>
+
+<style>
+@import "../assets/login.css";
+</style>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="register">
+    <h2 class="register__title">Регистрация</h2>
+    <input class="register__data" id="login" placeholder="Login">
+    <input class="register__data" id="password" placeholder="Password">
+    <div class="register__issue">
+    <p class="register__issue_text">Уже есть акаунт?</p>
+      <nuxt-link class="register__issue_link" to="/login">Вход</nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+definePageMeta({
+  layout: 'empty'
+})
+</script>
+
+<style>
+@import "../assets/register.css";
+</style>
